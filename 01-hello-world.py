948db3ea86d46b97f3eecb9435552fe50a9a9085
Jose Ignacio Galarza <igalarzab@gmail.com>
2014-06-27 01:56:18 +0200

    Explain example 1

--- 01-hello-world.py
+++ 01-hello-world.py
@@ -7,11 +7,16 @@
 #   Just demonstrate how to print a hello world
 #   using asyncio's eventloop.
 #
+# NOTE:
+#   This example is a little bit silly because say_hello
+#   is a normal function and **not** a real coroutine. It's
+#   just to show a hello-world as simple as possible using
+#   asyncio event loop
 
 import asyncio
 
 
-# The decorator will convert the func to a coroutine
+# The decorator will convert the func to a coroutine (see NOTE above)
 @asyncio.coroutine
 def say_hello():
     print('Hello world!')
